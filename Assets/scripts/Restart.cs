a6b33e4a24324f53440118dcf339d629acd79aa2
Someone love this era ! <lsmfeixiang@163.com>
2015-12-07 20:12:28 +0800

    unity3d学习项目

---
+++ Assets/scripts/Restart.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Restart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public AudioSource music;
+    void OnGUI()
+    {
+        if (GUILayout.Button("退出"))
+        //if (GUILayout.Button(new Rect(0, 0, 60, 30), "退出"))
+            {
+            //Application.Quit();
+            print("退出");
+        }
+        if (GUILayout.Button("重新开始"))
+        //if (GUILayout.Button(new Rect(0, 100, 60, 30), "重新开始"))
+        {
+            Application.LoadLevel("01");
+        }
+        if(GUILayout.Button("Play"))
+        //if(GUILayout.Button(new Rect(10, 160, 100, 50), "Play"))
+        {
+            music.Play();            
+        }
+        if (GUILayout.Button("Pause"))
+        //if (GUILayout.Button(new Rect(10, 160, 100, 50), "Pause"))
+        {
+            music.Pause();
+        }
+        if (GUILayout.Button("Stop"))
+        //if (GUILayout.Button(new Rect(10, 160, 100, 50), "Stop"))
+        {
+            music.Stop();
+        }
+        print("ongui");
+        music.Play();
+    }
+}
---
+++ Assets/scripts/ShotIt.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotIt : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+    public int zidancount = 0;
+
+    public int destoryobjectcount = 0;
+
+    public float speed = 5;
+    // Update is called once per frame
+    public Transform original;
+    void Update () {
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        float z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        transform.Translate(x, 0, z);
+       
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            print("Fire");
+            Transform zidan = Instantiate(original, transform.position, transform.rotation) as Transform;
+            Vector3 forward = transform.TransformDirection(Vector3.forward);
+            //zidan.rigidbody;
+            Rigidbody rb = zidan.GetComponent<Rigidbody>();
+            rb.AddForce(forward * 2800);
+
+            zidancount++;
+            GameObject.Find("Canvas/zidancount").GetComponent<UnityEngine.UI.Text>().text = "子弹数：" + zidancount +"   消灭数："+destoryobjectcount;
+        }
+
+        //镜头旋转
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(0, -25 * Time.deltaTime, 0, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(0, 25 * Time.deltaTime, 0, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.Z))
+        {
+            transform.Rotate(0, 0, 25 * Time.deltaTime, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.X))
+        {
+            transform.Rotate(0, 0, -25 * Time.deltaTime, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.F))
+        {
+            transform.Rotate(25 * Time.deltaTime, 0, 0, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.G))
+        {
+            transform.Rotate(-25 * Time.deltaTime, 0, 0, Space.Self);
+        }
+    }
+}
