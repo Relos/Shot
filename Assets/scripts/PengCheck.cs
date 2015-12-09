67f7eefa5b1b88ef80b505d2075f245b9f9c731b
unknown <menglu@ITA-1202-2010.synacast.local>
2015-12-09 14:03:54 +0800

    add a new scene:start menu

---
+++ Assets/scripts/PengCheck.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PengCheck : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter()
+    {
+        GameObject.Find("Canvas/tips").GetComponent<UnityEngine.UI.Text>().text = "Hey,U hit me!";
+        GameObject.Find("Spotlight").GetComponent<Light>().enabled = false;
+        GameObject.Find("arealight").GetComponent<Light>().enabled = false;
+        GameObject.Find("pingxinglight").GetComponent<Light>().enabled = false;
+    }
+}
---
+++ Assets/scripts/StartMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnGUI()
+    {
+        if(GUI.Button(new Rect(380,350,60,30), "退出"))
+        {
+            Application.Quit();
+        }
+        if (GUI.Button(new Rect(480, 350, 60, 30), "开始游戏"))
+        {
+            Application.LoadLevel("Shot");
+        }
+    }
+}
